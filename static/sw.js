81cbab0ad5652b318b4a276e0f58469cb5ffea92
nitta-kazuo <xnitta.kazuo@contract.isid.co.jp>
2018-12-11 12:14:22 +0900

    dependenciesを初期状態に戻した版

--- static/sw.js
+++ static/sw.js
@@ -2,6 +2,10 @@ importScripts('/_nuxt/workbox.dev.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
+    "url": "/_nuxt/0e8b777dd07d5765f8b2.js",
+    "revision": "cbc91134a16bdc61366dd948390d4cf2"
+  },
+  {
     "url": "/_nuxt/19db677b5b6e8c3f07f6.js",
     "revision": "548ef70b2013a6ecf4f3179be14214b5"
   },
@@ -10,27 +14,23 @@ workbox.precaching.precacheAndRoute([
     "revision": "8ed62a73d210a3c89e66ce73396afce3"
   },
   {
-    "url": "/_nuxt/5bfcc459217a05453524.js",
-    "revision": "f44ab31b2ea96eb4cc409846ee647b60"
+    "url": "/_nuxt/5be813b184b976e974b1.js",
+    "revision": "40b360fce7dca0562ee957de20c8d902"
   },
   {
-    "url": "/_nuxt/63a441a1852019e06763.js",
-    "revision": "ec738f580de7a9985be453110bb393f6"
+    "url": "/_nuxt/5bfcc459217a05453524.js",
+    "revision": "f44ab31b2ea96eb4cc409846ee647b60"
   },
   {
     "url": "/_nuxt/6fc3780f62dfb7095105.js",
     "revision": "b5eb1018e9d1971d4c81f00571b2267e"
   },
   {
-    "url": "/_nuxt/7b0c7f6f9f60f6bb085f.js",
-    "revision": "9c7d620760c4ddfccb5d5e62b90f28d9"
-  },
-  {
-    "url": "/_nuxt/8faca668de11ae75a5f1.js",
-    "revision": "93aaade981ff6b769186334be964ea3a"
+    "url": "/_nuxt/c9154330067bff69ecf7.js",
+    "revision": "15e60c2638cfb3fdba3dd853e74add9f"
   }
 ], {
-  "cacheId": "nuxt-pwa-fb-vuetify01",
+  "cacheId": "nuxt-pwa-fb-vuetify02",
   "directoryIndex": "/",
   "cleanUrls": false
 })
